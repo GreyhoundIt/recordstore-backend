aec5221342f94feec9716854aeae73c82694bce1
Matthew Hargraves <matthew.hargraves@cef.co.uk>
2019-03-15 13:18:58 +0000

    controllers done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+  include JWTSessions::RailsAuthorization
+  rescue_from JWTSessions::Errors::Unauthorized, with: :not_authorised
+
+  private
+
+  def current_user
+    @current_user ||= User.find(payload['user_id'])
+  end
+
+  def not_authorised
+    render json: { error: 'Not Authorised' }, status: :unauthorized
+  end
 end
